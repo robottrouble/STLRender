8113ce653ae70dd647b2354c47c5e96e60557eb9
Adam Kaufman <unmotivated@gmail.com>
2011-08-03 11:01:01 -0400

    importing sources

---
+++ render_stl.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+STL=$1
+TMP=$2
+START_DIR=`dirname $0`
+BASEFILE=`echo "$STL" | sed -e 's/\.stl//g'`
+OUTFILE=${BASEFILE}.png
+POVFILE=${TMP}/`basename ${BASEFILE}.pov`
+INCFILE=${TMP}/`basename ${BASEFILE}.inc`
+
+${START_DIR}/stl2pov -s "$STL" > "${INCFILE}"
+MODELNAME=`grep "#declare" "${INCFILE}" | cut -d\  -f2`
+echo $INCFILE
+cat ${START_DIR}/pov_layout.tmpl | sed -e "s@{{INCLUDE_FILE}}@${INCFILE}@g" -e "s@{{MODELNAME}}@${MODELNAME}@g" -e "s@{{BASE_PATH}}@${START_DIR}@g" > "${POVFILE}"
+	   
+# cat "${POVFILE}"
+povray -s -i"${POVFILE}" +FN +W1600 +H1200 -o"${OUTFILE}" +Q9 +AM2 +A2
+echo "OUTFILE: ${OUTFILE}"
